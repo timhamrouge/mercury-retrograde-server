4a80f54bfe6667da4969dd4675298e8b3d4a1f50
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2022-10-09 17:56:25 +0100

    it seeds a date

---
+++ models/dates.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const DateSchema = new Schema({
+  start_date: {
+    type: Date,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("dates", DateSchema);
--- seed.js
+++ seed.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
 mongoose.Promise = Promise;
 require('dotenv').config();
+const dateData = require("./seed/data/retrograde.json");
+const Dates = require("./models/dates")
 
 mongoose.connect(process.env.DB_URL).then(() => {
   console.log(`connected to ${process.env.DB_URL}`)
-  console.log(mongoose.connection.db)
+  return mongoose.connection.dropDatabase()
+  .then(() => {
+    return Promise.all([
+      Dates.insertMany(dateData)
+    ])
+  })
 }).then(() => {
   console.log('disconnecting')
   mongoose.disconnect()
