9957b7779e9b453994506787b792737234fa1351
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2022-10-13 19:05:19 +0100

    it works without any weird hour subtraction

--- controllers/retrogradeController.js
+++ controllers/retrogradeController.js
@@ -1,32 +1,21 @@
 const dates = require("../models/dates");
 
 function getRetrogradeByDate(req,res,next) {
-  // this is subtracting an hour
-  const isoDate = new Date(req.query.date)
-  console.log('1', isoDate)
+  // pass in date as YYYY-mm-DD
+
   return dates.findOne({
     $and: [{
       'start_date' : {
-        $lte: isoDate
+        $lte: req.query.date
       },
       'end_date' : {
-        $gte: isoDate
-
+        $gte: req.query.date
       }
     }]
-    // start_date: {
-    //   $gte: isoDate
-    // },
-    // end_date: {
-    //   $lte: isoDate
-    // }
   }).lean().then((retrograde) => {
-  console.log('2', retrograde)
 
     return Promise.all([retrograde])
   }).then((x) => {
-  console.log('3', x)
-
     return res.send({x})
   })
  }
