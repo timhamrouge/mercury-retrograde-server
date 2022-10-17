abd9cfd9b9bf5dba2b02c3e5eb603dbebb4df790
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2022-10-17 17:20:26 +0100

    trash commit

--- controllers/retrogradeController.js
+++ controllers/retrogradeController.js
@@ -1,27 +1,42 @@
 const dates = require("../models/dates");
 // make a reusable method
-function getRetrogradeByDate(req,res,next) {
+async function getRetrogradeByDate(req,res,next) {
   // pass in date as YYYY-mm-DD
 
-  return dates.findOne({
-    $and: [{
-      'start_date' : {
-        $lte: req.query.date
-      },
-      'end_date' : {
-        $gte: req.query.date
-      }
-    }]
-  }).lean().then((retrograde) => {
-
-    return Promise.all([retrograde])
-  }).then((retrograde) => {
-    let result = true;
-    if (retrograde.includes(null)) result = false;
-    return res.send({isRetrogrde: result})
-  }).catch(err => {
+  try {
+    return await dates.findOne({
+      $and: [{
+        'start_date' : {
+          $lte: req.query.date
+        },
+        'end_date' : {
+          $gte: req.query.date
+        }
+      }]
+    })
+  } catch {
     next(err);
-  })
+  }
+
+  // return dates.findOne({
+  //   $and: [{
+  //     'start_date' : {
+  //       $lte: req.query.date
+  //     },
+  //     'end_date' : {
+  //       $gte: req.query.date
+  //     }
+  //   }]
+  // }).lean().then((retrograde) => {
+
+  //   return Promise.all([retrograde])
+  // }).then((retrograde) => {
+  //   let result = true;
+  //   if (retrograde.includes(null)) result = false;
+  //   return res.send({isRetrogrde: result})
+  // }).catch(err => {
+  //   next(err);
+  // })
  }
 
  function getRetrogradeTweetByDate(req,res,next) {
