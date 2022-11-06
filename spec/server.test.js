00088ba1926f18e61b7c8a87219de85aa7d1edaf
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2022-11-06 20:23:55 +0000

    todo in specs

--- spec/server.test.js
+++ spec/server.test.js
@@ -1,10 +1,13 @@
 const app = require("../server"); // Link to your server file
 const supertest = require("supertest");
 const request = supertest(app);
+const mongoose = require("mongoose");
 
-// afterEach(() => {
-//   mongoose.disconnect();
-// });
+// figure out how to supress the 'Jest did not exit one second after the test run has completed.' issue
+
+afterAll(() => {
+  app.clo
+});
 
 it("Gets the test endpoint", async () => {
   // Sends GET Request to /test endpoint
@@ -12,7 +15,6 @@ it("Gets the test endpoint", async () => {
 
   expect(res.status).toBe(200);
   expect(res.text).toBe("Hello Timbo");
-
 });
 
 
