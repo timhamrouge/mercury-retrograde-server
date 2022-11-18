f6ae4b661789cde5f3f22f1c16fda5d580fa37e3
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2022-11-18 10:06:48 +0000

    lint seed

--- seed.js
+++ seed.js
@@ -1,21 +1,21 @@
-const mongoose = require("mongoose");
-mongoose.Promise = Promise;
-require('dotenv').config();
-const dateData = require("./seed/data/retrograde.json");
-const Dates = require("./models/dates")
+const mongoose = require('mongoose')
+mongoose.Promise = Promise
+require('dotenv').config()
+const dateData = require('./seed/data/retrograde.json')
+const Dates = require('./models/dates')
 
 mongoose.connect(process.env.DB_URL).then(() => {
   console.log(`connected to ${process.env.DB_URL}`)
   return mongoose.connection.dropDatabase()
-  .then(() => {
-    return Promise.all([
-      Dates.insertMany(dateData)
-    ])
-  })
+    .then(() => {
+      return Promise.all([
+        Dates.insertMany(dateData)
+      ])
+    })
 }).then(() => {
   console.log('disconnecting')
   mongoose.disconnect()
 }).catch(err => {
-  console.log(err);
-  mongoose.disconnect;
+  console.log(err)
+  mongoose.disconnect()
 })
