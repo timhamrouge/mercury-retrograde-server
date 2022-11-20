8ddb2db850994dc09fa44d70f910fdab8800ce97
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2022-11-20 20:35:55 +0000

    lint server

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 const express = require('express')
 const app = express()
 
-const mongoose = require("mongoose");
-require('dotenv').config();
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-const {getBooleanValueForDate,getRetrogradeDatesByDate, getRetrogradeTweetByDate} = require("./controllers/retrogradeController")
+const { getBooleanValueForDate, getRetrogradeDatesByDate, getRetrogradeTweetByDate } = require('./controllers/retrogradeController')
 
-app.use(express.json());
+app.use(express.json())
 
 mongoose.connect(process.env.DB_URL).then(() => {
-  console.log(`connected to ${process.env.DB_URL}`);
-});
+  console.log(`connected to ${process.env.DB_URL}`)
+})
 
 app.get('/', function (req, res) {
   res.send('Hello Timbo')
 })
 
-app.get('/retrograde-dates', getRetrogradeDatesByDate);
+app.get('/retrograde-dates', getRetrogradeDatesByDate)
 
 app.get('/is-mercury-retrograde',
-getBooleanValueForDate
-);
+  getBooleanValueForDate
+)
 
 app.get('/tweet', getRetrogradeTweetByDate)
 
 app.listen(3000)
 
-module.exports = app;
+module.exports = app
